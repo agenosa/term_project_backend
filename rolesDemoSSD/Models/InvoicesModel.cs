42951935fb95001d12068d30d6f9bbfbcdf22abc
Ross Scharbach <rscharbach1@my.bcit.ca>
2022-02-08 11:36:26 -0800

    added model classes

---
+++ rolesDemoSSD/Models/InvoicesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rolesDemoSSD.Models
+{
+    public class InvoicesModel
+    {
+        public int InvoiceID { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public decimal InvoiceTotal { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
